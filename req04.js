d59c43e6c38c88051eaa6789b4cd63a110ff50a1
BFulop91 <79876163+BFulop91@users.noreply.github.com>
2021-03-02 23:53:38 +0100

    Delete req04.js

--- req04.js
+++
@@ -1,8 +0,0 @@
-describe('Home button turn to active when click on it', () => {
-  it('Visit UI Test page', () => {
-	cy.visit('http://uitest.duodecadits.com/form.html')
-	cy.contains('Home').click();
-	cy.contains('Home').should('have.css','background-color','rgb(8, 8, 8)');
-
-  })
-})
