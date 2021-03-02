b5296994ae5ad692937eee6c0f19261e25dce2a9
BFulop91 <79876163+BFulop91@users.noreply.github.com>
2021-03-02 23:54:21 +0100

    Delete req09.js

--- req09.js
+++
@@ -1,8 +0,0 @@
-describe('<h1> tag text on Home page', () => {
-  it('Visit UI Test page', () => {
-	cy.visit('http://uitest.duodecadits.com')
-	//'Welcome to the Docler Holding QA Department' text visible on the Home page in <h1> tag
-	cy.get('h1').contains('Welcome to the Docler Holding QA Department');
-
-  })
-})
