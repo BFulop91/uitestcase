eeefdbed60ec798183d472fd52bf2f2dda40b458
BFulop91 <79876163+BFulop91@users.noreply.github.com>
2021-03-02 23:54:33 +0100

    Delete req10.js

--- req10.js
+++
@@ -1,8 +0,0 @@
-describe('<p> tag text on Home page', () => {
-  it('Visit UI Test page', () => {
-	cy.visit('http://uitest.duodecadits.com')
-	cy.get('p').contains('This site is dedicated to perform some exercises and demonstrate automated web testing.');
-
-	
-  })
-})
